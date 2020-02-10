1183e0940c4134df1ddbee6e5bb5502c2df75870
John Ivens <ivensj@capuchin.localdomain>
2020-02-10 04:50:27 -0700

    Fixed auth0, probably not through code changes but by doing an npm update

--- app-boilerplate/src/components/Auth/auth0-variables.js
+++ app-boilerplate/src/components/Auth/auth0-variables.js
@@ -1,5 +1,12 @@
 export const AUTH_CONFIG = {
   domain: 'annotaurus-lex.auth0.com',
   clientId: 'bQSzTBGSSw2lgLsycCe7oI5beimOmkOb',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback',
+  leeway: 300,
+  oidcClient: {
+    // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
+    httpTimeout: 2500,
+    // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
+    clockTolerance: 1000000
+  }
 };
